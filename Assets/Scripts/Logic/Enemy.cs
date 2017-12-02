14246ae3993a8f8eb9677a525ed1d4a5925c70f9
golergka <golergka@gmail.com>
2017-12-02 22:46:52 +0300

    Enemies are now kamikadze

--- Assets/Scripts/Logic/Enemy.cs
+++ Assets/Scripts/Logic/Enemy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-[RequireComponent(typeof(PeriodicDamager))]
+[RequireComponent(typeof(Damager))]
 public class Enemy : MonoBehaviour
 {
 	#region Components
@@ -16,12 +16,12 @@ public class Enemy : MonoBehaviour
 		}
 	}
 
-	private PeriodicDamager damageDealer;
-	private PeriodicDamager PeriodicDamager
+	private Damager damageDealer;
+	private Damager Damager
 	{
 		get
 		{
-			return damageDealer ?? (damageDealer = GetComponent<PeriodicDamager>());
+			return damageDealer ?? (damageDealer = GetComponent<Damager>());
 		}
 	}
 
@@ -29,19 +29,15 @@ public class Enemy : MonoBehaviour
 
 	public int GoldReward;
 
+	IDamageReceiver target;
 	Transform targetTransform;
 
 	public void Init<T>(T target)
 		where T : Component, IDamageReceiver
 	{
+		this.target = target;
 		targetTransform = target.transform;
-		PeriodicDamager.Target = target;
-		RebuildPath();
-	}
-
-	public void RebuildPath()
-	{
-		NavMeshAgent.SetDestination(targetTransform.position);
+		currentState = new MovementState(this);
 	}
 
 	void OnDrawGizmosSelected()
@@ -50,11 +46,92 @@ public class Enemy : MonoBehaviour
 		Gizmos.DrawLine(transform.position, targetTransform.position);
 	}
 
+	#region States
+	
+	EnemyState currentState;
+
+	abstract class EnemyState
+	{
+		protected readonly Enemy enemy;
+
+		public EnemyState(Enemy enemy)
+		{
+			this.enemy = enemy;
+		}
+
+		public abstract void Update();
+		public abstract void RebuildPath();
+	}
+
+	class MovementState : EnemyState
+	{
+		public MovementState(Enemy enemy) : base(enemy)
+		{
+			RebuildPath();
+		}
+
+		public override void Update() 
+		{ 
+			var targetDelta = enemy.targetTransform.position - enemy.transform.position;
+			var closeEnough = targetDelta.sqrMagnitude <= Mathf.Pow(enemy.NavMeshAgent.stoppingDistance, 2f);
+
+			if (closeEnough)
+			{
+				enemy.currentState = new KamikadzeState(enemy);
+			}
+		}
+
+		public override void RebuildPath()
+		{
+			enemy.NavMeshAgent.SetDestination(enemy.targetTransform.position);
+		}
+	}
+
+	class KamikadzeState : EnemyState
+	{
+		readonly float startTime;
+		const float KAMIKADZE_TIME = 1f;
+
+		public KamikadzeState(Enemy enemy) : base(enemy)
+		{
+			startTime = Time.time;
+			enemy.Damager.DamageInstantly(enemy.target);
+		}
+
+		public override void Update()
+		{
+			if (Time.time - startTime >= KAMIKADZE_TIME)
+			{
+				Destroy(enemy.gameObject);
+			}
+		}
+
+		public override void RebuildPath()
+		{
+			// Do nothing
+		}
+	}
+
+	#endregion
+
+	#region State delegates
+
+	public void RebuildPath()
+	{
+		if (currentState != null)
+		{
+			currentState.RebuildPath();
+		}
+	}
+
 	void Update()
 	{
-		var targetDelta = targetTransform.position - transform.position;
-		var closeEnough = targetDelta.sqrMagnitude <= Mathf.Pow(NavMeshAgent.stoppingDistance, 2f);
-
-		PeriodicDamager.enabled = closeEnough;
+		if (currentState != null)
+		{
+			currentState.Update();
+		}
 	}
+
+	#endregion
+
 }
